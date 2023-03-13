b3e69f3d498cf556a7c0d5b55bf4f2917d55eddb
TommasoMarrese77 <127745720+TommasoMarrese77@users.noreply.github.com>
2023-03-13 16:48:52 +0100

    Add files via upload

---
+++ tommaso.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int x;
+int y;
+
+printf("inserisci un numero:");
+scanf("%d", &x);
+printf("il numero inserito è %d\n", x);
+scanf("%d", &y);
+printf("i numeri inseriti sono x= %d e y= %d\n", x, y);
+} 
