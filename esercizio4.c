baf42c50812213ee217bb5b10ba173dd6a3b3d22
TommasoMarrese77 <127745720+TommasoMarrese77@users.noreply.github.com>
2023-03-14 15:10:58 +0100

    Add files via upload

--- esercizio4.c
+++ esercizio4.c
@@ -8,6 +8,14 @@ int main() {
         if (lato2+lato3 > lato1) {
             if (lato3+lato1 > lato2) {
                 printf("\033[0;32m%d, %d, %d sono lati di un triangolo\n\033[0;0m", lato1, lato2, lato3);
+                if (lato1 == lato2 && lato2 == lato3 && lato1 == lato3) {
+                        printf("\033[0;35m%d, %d, %d sono i lati di un triangolo equilatero\033[0;35m\n", lato1, lato2, lato3);
+                } 
+                else if (lato1 != lato2 && lato2 != lato3 && lato1 != lato3) {
+                    printf("\033[0;36m%d, %d, %d sono i lati di un triangolo scaleno\n\033[0;0m", lato1, lato2, lato3);
+                } else {
+                    printf("\033[0;33m%d, %d, %d sono i lati di un triangolo isoscele\n\033[0;33m", lato1, lato2, lato3);
+                }
             } else {
                 printf("\033[0;31m%d, %d, %d non sono lati di un triangolo\n\033[0;0m", lato1, lato2, lato3);
             }
